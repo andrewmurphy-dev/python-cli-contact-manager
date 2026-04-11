94a691a1ae8e4bd1fa0b897410e5500502c980d1
Andrew Murphy <legionastartesofficial@gmail.com>
2026-04-11 23:08:58 +0900

    Update and rename main.py to main_cli.py

--- main.py
+++
@@ -1,6 +0,0 @@
-
-
-
-#im trying to write this better ! instead of saying print , print , print !
-
-
---
+++ main_cli.py
@@ -0,0 +1,94 @@
+from auth_cli import add_contact,search_contacts,display_contacts
+
+
+
+def cli_manager():
+    print("welcome to main menu\n")
+    name = input("Enter your name: ").strip().lower()
+
+
+    while True:
+        cli_menu = (f"{name} please enter your choice: ").strip().lower()
+        print(cli_menu)
+        #press 1 to add contact
+        print("press 1: to add contacts\n")
+        #press 2 to search contact
+        print("press 2: to search contacts\n")
+        #press 3 to update contact
+        print("press 3: to update contacts\n")
+        #press 4 tp delete contact
+        print("press 4: to delete contacts\n")
+        #press 5 to show contacts
+        print("press 5: to display contacts\n")
+        # press 'exit to leave
+        print("press 6: to exit\n")
+
+        user_inputt = input("-->:  ").strip().lower()
+        if user_inputt == "exit":
+            print("goodbye")
+            break
+
+
+
+        if user_inputt == "1":
+            print("menu: 1 selected")
+            add_contact()
+        elif user_inputt == "":
+            print("menu: option cannot be blank! try again !")
+
+        else:
+            print("invalid option")
+
+
+
+        if user_inputt == "2":
+            print("menu: 2 selected")
+            search_contacts()
+        elif user_inputt == "":
+            print("menu: option cannot be blank! try again !")
+
+        else:
+            print("invalid option")
+
+        if user_inputt == "3":
+            print("menu: 3 selected")
+        elif user_inputt == "":
+            print("menu: option cannot be blank! try again !")
+
+        else:
+            print("invalid option")
+
+        if user_inputt == "4":
+            print("menu: 4 selected")
+        elif user_inputt == "":
+            print("menu: option cannot be blank! try again !")
+
+        else:
+            print("invalid option")
+
+
+        if user_inputt == "5":
+            print("menu: 5 selected")
+            display_contacts()
+        elif user_inputt == "":
+            print("menu: option cannot be blank! try again !")
+
+        else:
+            print("invalid option")
+
+
+
+
+
+
+
+
+cli_manager()
+add_contact()
+search_contacts()
+display_contacts()
+
+
+#im trying to write this better ! instead of saying print , print , print !
+
+
