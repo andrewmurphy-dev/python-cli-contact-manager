dfb3a9ee0066448bb1d33ec837f3f6447d84ff07
Andrew Murphy <legionastartesofficial@gmail.com>
2026-06-05 08:02:19 +0900

    refactor

--- auth_cli.py
+++ auth_cli.py
@@ -1,49 +1,147 @@
 
 from storage_cli import contacts
 
-
-def add_contact():
-    print("welcome to add contact section!")
+def add_contact_api():
+    print()
+    print("Welcome to add contact section!")
 
     name = input("Please enter your name: ").strip().lower()
-    email = input("Please enter your email: ").strip().lower()
-    phone = input("Please enter your phone number: ").strip()
 
     if name == "":
-        print("name cannot be blank")
-        return
+        print("name cannot be blank!")
+        return 
+    
+    email = input("Please enter your email: ").strip().lower()
 
-    contacts[name] = {"email": email, "phone": phone}
+    if email == "":
+        print("email must not be blank!")
 
-    print("contact added")
-    print(contacts)
+
+    phone = input("Please enter your phone number: ").strip()
+
+    if len(phone) < 8:
+        print("phone is too short!")
+        return 
+    
+    #we want the number to be a string !but we want it to be digit strings !
+    #not character stirngs !
+    
+
+    for character in phone:
+       if not character.isdigit():
+           print("phone number must be digits!")
+           return 
+       
+
+    contacts = {
+        "name": name,
+        "email": email,
+        "phone": phone
+    }
+       
+
+    try:
+        response = request.post(f"{BASE_URL}/contact",
+                                json = {"name": name, 
+                                        "email": email,
+                                        "phone": phone}
+                                        , timeout=10
+                    
+                                )
+        
+
+        response.raise_for_status()
+        data = response.json()
+        print(data["message"])
+
+
 
 
 
 #day 2
 
 def display_contacts():
-    print("welcome to display contacts!")
-    variable = input("menu: please enter your choice")
-    print("press 1: \tto view all users")
+
+    try:
+        response = requests.get(f"{BASE_URL}/Contact", Timeout=10)
+
+        response.raise_for_status()
+
+        for contact_key, contact_value in contact.items():
+            print("name:", contact_value["name"])
+            print("phone:", contact_value["phone"])
+            print("email:", contact_value["email"])
+            print()
+
+    
+    except requests.exceptions.ConnectionError:
+        print("connection to the server was not viable!")
+        return 
+
+
+    except request.exceptions.Timout:
+        print("error: server timout exceeded!")
+        return
+
+
+    except request.exceptions.HTTPerror as error:
+        print(f"error: server has returned {error}")
+        return
+
+
+    except request.exceptions.RequestException:
+        print("error: server request failed!")
+        return
+
+
+    except ValueError:
+        
+
     
 
 
 
-    #day 3
-def search_contacts():
-    print("wecome to search menu")
-    email = input("menu: please enter the email address associated with user: ")
-    for name, value in contacts.items():
-        if email == value["email"]:
-            print("username:\t", name)
-            print("email:\t", value["email"])
-            print("phone:\t", value["phone"])
-            break
-    else:
-         print("not found")
 
-        #what happens if the user types
+def search_contacts():
+    print() 
+    print("welcome to search contacts!")
+
+    email = input("enter the associated email address of the user in which you want to search: ")
+
+
+    try:
+        response = requests.get(f"{BASE_URL}/contacts/{email}", timeout=10,
+                               json = {
+                                   "email": email
+                               }
+        )
+
+        response.raise_for_status()
+
+        data = response.json() 
+        print(data)
+        
+    
+    except requests.exceptions.Connection.Error:
+        print("error: unable to connect to server")
+        return 
+    
+
+    except requests.exception.Timeout:
+        print("error: the server has exceeded the timeout!")
+        return 
+
+
+    except requests.exception.HTTPerror as error:
+        print(f"error: the server has an {error}")
+        return 
+    
+    except requests.exception.Request.Exception:
+        print("error: server request failed!")
+        return 
+
+
+
 
 
 def delete_contact():
