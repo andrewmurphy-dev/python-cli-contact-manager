aced6e09ad32349accf3f0be4ea263db6d110118
Andrew Murphy <legionastartesofficial@gmail.com>
2026-06-07 01:22:02 +0900

    implement update_api

--- auth_cli.py
+++ auth_cli.py
@@ -144,15 +144,40 @@ def search_contacts():
 
 
 
-def delete_contact():
-    print("welcome to delete contact")
-    name = input("please enter the username you want to delete: ")
-    if name in contacts:
-        del contacts[name]
-        print("contact deleted")
-    else:
-        print("not found")
 
+def delete_contact():
+    print()
+    print("welcome to delete contact section!")
+
+    email = input("Enter the user email you wish to delete!: ").strip()
+
+    try:
+        response = requests.delete(f"{BASE_URL}/contact", timeout=10, 
+                                   json = {
+                                       "email": email
+                                   })
+        
+        response.raise_for_status()
+
+        data = response.json()
+        print(data)
+
+
+    except requests.exceptions.ConnectionError:
+        print("error: there is an error with connecting to the server!")
+        return
+    
+    except requests.exception.Timeout:
+        print("error: the server connection has exceeded the timeout!")
+        return 
+
+    except requests.exception.HTTPerror as error:
+        print(f"error: Server has an {error}")
+        return 
+
+    except requests.exception.Request.Exception:
+        print("error: server request failed!")
+        return 
 
 
 
---
+++ delete_api.py
@@ -0,0 +1,37 @@
+
+
+def delete_contact():
+    print()
+    print("welcome to delete contact section!")
+
+    email = input("Enter the user email you wish to delete!: ").strip()
+
+    try:
+        response = requests.delete(f"{BASE_URL}/contact", timeout=10, 
+                                   json = {
+                                       "email": email
+                                   })
+        
+        response.raise_for_status()
+
+        data = response.json()
+        print(data)
+
+
+    except requests.exceptions.ConnectionError:
+        print("error: there is an error with connecting to the server!")
+        return
+    
+    except requests.exception.Timeout:
+        print("error: the server connection has exceeded the timeout!")
+        return 
+
+    except requests.exception.HTTPerror as error:
+        print(f"error: Server has an {error}")
+        return 
+
+    except requests.exception.Request.Exception:
+        print("error: server request failed!")
+        return 
+
+    
